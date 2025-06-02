782e23c8e5f787d5ddcdf5c58008fcc14583cbc6
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-06-02 16:46:32 +0530

    sample 3 added

---
+++ vector/sample3.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printSortedwithIndex(int arr[],int n)
+{
+    vector<pair<int,int>>v;
+    for(int i=0;i<n;i++)
+    {
+        v.push_back({arr[i],i});
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i].first<<" "<<v[i].second;
+        cout<<endl;
+    }
+}
+int main()
+{
+    int arr[]={20,10,5,40};
+    printSortedwithIndex(arr,4);
+    return 0;
+}
